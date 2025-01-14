4fc9408c0b9b1c6f9820f9d1a48d1c31d2d0e89f
Jade-m22 <mj.micheljade@gmail.com>
2025-01-14 09:17:37 +0100

    add exos 15 à 17

---
+++ exo_15.rb
@@ -0,0 +1,12 @@
+puts "Donne moi un nombre entre 1 et 25"
+number = gets.chomp.to_i
+
+while number < 1 || number > 25
+  puts "Le nombre n'est pas entre 1 et 25"
+  print "> "
+  number = gets.chomp.to_i
+end
+
+1.upto(number) do |etages|
+  puts "#" * etages
+end
---
+++ pyramide.rb
@@ -0,0 +1,13 @@
+puts "Donne moi un nombre entre 1 et 25"
+number = gets.chomp.to_i
+
+while number < 1 || number > 25
+  puts "Le nombre n'est pas entre 1 et 25"
+  print "> "
+  number = gets.chomp.to_i
+end
+
+1.upto(number) do |etages|
+  espaces = number - etages 
+  puts " " * espaces + "#" * etages
+end
